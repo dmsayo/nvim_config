f4cdf157e6cfc3d116c454d35e8c785368b3fcc3
Derryl Sayo <derryl@formswim.com>
2023-09-07 13:57:20 -0700

    Change type definition shortcut

--- nvim/init.lua
+++ nvim/init.lua
@@ -434,7 +434,7 @@ local on_attach = function(_, bufnr)
     '[G]oto [D]efinition [S]plit')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
   nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+  nmap('<leader>td', vim.lsp.buf.type_definition, '[T]ype [D]efinition')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
